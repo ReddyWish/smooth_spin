283fbe8f49756059499aa38b719ea0c9b2c3c1a4
ilya <ilya.shuvatov1@gmail.com>
2025-01-04 17:43:41 +0100

    Connect neon database with drizzle

---
+++ data/tickets.sql
@@ -0,0 +1,18 @@
+INSERT INTO tickets (
+    customer_id, title, description, completed, tech, created_at, updated_at
+) VALUES
+    (1, 'Flat Tire Repair', 'Customer reported a flat tire that needs to be repaired.', false, 'unassigned', now(), now()),
+    (1, 'Brake Adjustment', 'Customer mentioned that the bike brakes are not functioning properly and need adjustment.', false, 'unassigned', now(), now()),
+    (1, 'Gear Shifting Issues', 'Customer is experiencing problems with the bike''s gear shifting.', false, 'unassigned', now(), now()),
+    (2, 'Chain Replacement', 'Customer needs a replacement for a worn-out bike chain.', false, 'unassigned', now(), now()),
+    (2, 'Wheel Truing', 'Customer reported that the bike wheel is wobbling and needs to be trued.', false, 'unassigned', now(), now()),
+    (2, 'Seat Adjustment', 'Customer requested a proper adjustment for their bike seat.', false, 'unassigned', now(), now()),
+    (3, 'Handlebar Alignment', 'Customer is experiencing discomfort due to misaligned handlebars.', false, 'unassigned', now(), now()),
+    (3, 'Suspension Check', 'Customer reported issues with the bike''s suspension system.', false, 'unassigned', now(), now()),
+    (3, 'Pedal Replacement', 'Customer needs to replace a damaged bike pedal.', false, 'unassigned', now(), now()),
+    (4, 'Lighting Installation', 'Customer requested installation of front and rear bike lights.', false, 'unassigned', now(), now()),
+    (4, 'Frame Inspection', 'Customer wants a detailed inspection of the bike frame for potential damage.', false, 'unassigned', now(), now()),
+    (4, 'Bell Installation', 'Customer requested installation of a new bike bell.', false, 'unassigned', now(), now()),
+    (5, 'Kickstand Repair', 'Customer''s bike kickstand is loose and needs repair.', false, 'unassigned', now(), now()),
+    (5, 'Fender Adjustment', 'Customer mentioned that the bike fender is misaligned and needs adjustment.', false, 'unassigned', now(), now()),
+    (5, 'Tire Replacement', 'Customer requested replacement of worn-out bike tires.', false, 'unassigned', now(), now());
